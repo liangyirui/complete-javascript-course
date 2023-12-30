a2d27c926f04e44e9e246aaf05aa2a483cc4d660
Yirui Liang <yirui@icloud.com>
2023-12-29 17:39:54 -0800

    01 fundamentals

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,41 @@
+// let country = "China";
+// let continent = "Asia";
+// let population = 14000;
+
+// console.log(country, continent, population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+console.log(null);
+
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const markBMI = markMass / markHeight ** 2;
+// const johnBMI = johnMass / johnHeight ** 2;
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+console.log("🧅");
+const year = '2023';
+console.log(Number(year) + 18);
+console.log(typeof Number('John'));
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log(23 - '10');
+
+//Truthy and Falsy values
+console.log(Boolean('H'));
+console.log(Boolean(undefined));
+console.log(Boolean(-1));
+
+// prompt function
+// const favorite = prompt("What's your favorite number?");
+// console.log(typeof favorite);
+// console.log('type' === 'type');
+
